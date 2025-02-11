22a03322613f055bfd63929e37901557230f0091
sharmiansi <sharmiansi@gmail.com>
2025-02-11 11:51:42 +0530

    update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739254728',resources: [
+    cacheName: 'chirpy-1739254881',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
